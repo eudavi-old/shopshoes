27d8be517b197a44fb12832a81e7d7c5862b8eca
mrhelder <helderacircletool@outlook.com>
2018-08-23 21:40:00 -0300

    CRUD Cliente

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :clientes
   resources :emails
   resources :funcionarios
   resources :dado_bancarios
---
+++ db/migrate/20180824003558_create_clientes.rb
@@ -0,0 +1,21 @@
+class CreateClientes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :clientes do |t|
+      t.string :cpf
+      t.string :nome
+      t.date :data_nasc
+      t.string :login
+      t.string :senha
+      t.string :cep
+      t.string :logradouro
+      t.float :debito
+      t.integer :num_logradouro
+      t.string :bairro
+      t.string :cidade
+      t.string :uf
+      t.string :pais
+
+      t.timestamps
+    end
+  end
+end
