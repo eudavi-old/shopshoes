98884e26a732b7d65bba3aa645882b86eefe0b20
Davi <davi.guimaraesleite@gmail.com>
2018-09-13 17:06:31 -0300

    static page home

--- app/models/telefone.rb
+++ app/models/telefone.rb
@@ -1,4 +1,3 @@
 class Telefone < ApplicationRecord
 	belongs_to :funcionario
-	accepts_nested_attributes_for :emails, allow_destroy: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
   resources :bancos
   resources :setors
   resources :cargos
-
-   root to: "funcionarios#index"
+  
+  get "/:page" => "pages#show"
+  root "pages#show", page: "home" 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
