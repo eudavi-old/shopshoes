7369b4b11b43259fb31abd357f7091639256fb8a
davigl <davi.guimaraesleite@gmail.com>
2018-11-11 17:30:46 -0200

    deploying

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,39 +12,3 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-
-unless Rails.env.development?
-  module SprocketsCachedLoader
-    def initialize(*)
-      super
-      @expanded_assets_cache = {}
-    end
-    
-    def asset_from_cache(key)
-      return @expanded_assets_cache[key] if @expanded_assets_cache.has_key?
-      
-      asset = cache.get(key, true)
-      
-      if asset
-        asset[:uri]       = expand_from_root(asset[:uri])
-        asset[:load_path] = expand_from_root(asset[:load_path])
-        asset[:filename]  = expand_from_root(asset[:filename])
-        asset[:metadata][:included]     = asset[:metadata][:included].map      { |uri| expand_from_root(uri) } if asset[:metadata][:included]
-        asset[:metadata][:links]        = asset[:metadata][:links].map         { |uri| expand_from_root(uri) } if asset[:metadata][:links]
-        asset[:metadata][:stubbed]      = asset[:metadata][:stubbed].map       { |uri| expand_from_root(uri) } if asset[:metadata][:stubbed]
-        asset[:metadata][:required]     = asset[:metadata][:required].map      { |uri| expand_from_root(uri) } if asset[:metadata][:required]
-        asset[:metadata][:dependencies] = asset[:metadata][:dependencies].map  { |uri| uri.start_with?("file-digest://") ? expand_from_root(uri) : uri } if asset[:metadata][:dependencies]
-
-        asset[:metadata].each_key do |k|
-          next unless k =~ /_dependencies\z/
-          asset[:metadata][k] = asset[:metadata][k].map { |uri| expand_from_root(uri) }
-        end
-        asset
-      end
-      
-      @expanded_assets_cache[key] = asset
-    end
-    
-    Sprockets::CachedEnvironment.prepend self
-  end
-end
