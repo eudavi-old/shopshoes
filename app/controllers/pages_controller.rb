7486f488ba7b1e73eb8071af479b6c0e669e5d95
davigl <davi.guimaraesleite@gmail.com>
2018-11-11 17:39:48 -0200

    fix home conflicts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
 	def show
 		@products = Product.last(6)
-		@product = Product.last
 		render template: "pages/#{params[:page]}", layout: false
 	end
 end
