19821ba2311d3856e5ed79b115f4207b1e8cd7ad
davigl <davi.guimaraesleite@gmail.com>
2019-05-17 13:34:42 -0300

    bank controller refactoring.

--- app/controllers/bancos_controller.rb
+++ app/controllers/bancos_controller.rb
@@ -1,73 +1,46 @@
 class BancosController < ApplicationController
   before_action :set_banco, only: [:show, :edit, :update, :destroy]
 
-  # GET /bancos
-  # GET /bancos.json
   def index
     @bancos = Banco.all
   end
 
-  # GET /bancos/1
-  # GET /bancos/1.json
-  def show
-  end
+  def show; end
 
-  # GET /bancos/new
   def new
     @banco = Banco.new
   end
 
-  # GET /bancos/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /bancos
-  # POST /bancos.json
   def create
     @banco = Banco.new(banco_params)
 
-    respond_to do |format|
-      if @banco.save
-        format.html { redirect_to @banco, notice: 'Banco was successfully created.' }
-        format.json { render :show, status: :created, location: @banco }
-      else
-        format.html { render :new }
-        format.json { render json: @banco.errors, status: :unprocessable_entity }
-      end
+    if @banco.save
+      redirect_to @banco, notice: 'Banco was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /bancos/1
-  # PATCH/PUT /bancos/1.json
   def update
-    respond_to do |format|
-      if @banco.update(banco_params)
-        format.html { redirect_to @banco, notice: 'Banco was successfully updated.' }
-        format.json { render :show, status: :ok, location: @banco }
-      else
-        format.html { render :edit }
-        format.json { render json: @banco.errors, status: :unprocessable_entity }
-      end
+    if @banco.update(banco_params)
+      redirect_to @banco, notice: 'Banco was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /bancos/1
-  # DELETE /bancos/1.json
   def destroy
     @banco.destroy
-    respond_to do |format|
-      format.html { redirect_to bancos_url, notice: 'Banco was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to bancos_url, notice: 'Banco was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_banco
       @banco = Banco.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def banco_params
       params.require(:banco).permit(:nome_banco)
     end
